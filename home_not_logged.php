ca1b44ab203a442284f2164cb712d589785298a2
Edouard <rouzedouard@yahoo.fr>
2023-04-13 12:35:03 +0200

    [FTR] - Création de la page d'accueil

---
+++ home_not_logged.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link href="assets/css/style.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+	<div class="containerHome">
+		<img id="accueilImg" src="assets/img/accueil3.jpg">
+	</div>
+	<div class="containerCategories" >
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/atelier.png" width="90px">
+			<h5 class="categoriesTitle">Ateliers</h5>
+			<p class="categoriesText">des ateliers variés sur site pour apprendre à cuisiner de bons plats ou découvrir de nouvelles recettes salées ou de pâtisserie, assurés par des professionnels</p>
+
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/chef.png" width="90px">
+			<h5 class="categoriesTitle">Cours</h5>
+			<p class="categoriesText">Apprenez à votre rythme dans leconfort de votre foyer grâce à nos cours à domicile personnalisés</p>
+
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/lecon.png" width="90px">
+			<h5 class="categoriesTitle">Leçon</h5>
+			<p class="categoriesText">Des leçons de cuisine disponibles directement sur notre site pour apprendre de nouvelles recettes </p>
+
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/vente.png" width="90px">
+			<h5 class="categoriesTitle">Vente</h5>
+			<p class="categoriesText">Que vous soyez un chef passionné ou un cuisinier débutant, notre large sélection de matériel de cuisine de qualité professionnelle vous permettra de réaliser vos recettes les plus créatives avec facilité</p>
+
+		</div>
+		<div class="elementCategories" id="">
+			<img src="assets/img/vente.png" width="90px">
+			<h5 class="categoriesTitle">Dégustation</h5>
+			<p class="categoriesText">Découvrez de nouvelles saveurs tout en soutenant une agriculture durable avec nos dégustations de produits bio soigneusement sélectionnés</p>
+			
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/formations.png" width="90px">
+			<h5 class="categoriesTitle">Formations</h5>
+			<p class="categoriesText">Des formations professionnelles pour se reconvertir dans la restauration</p>
+
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/order.png" width="90px">
+			<h5 class="categoriesTitle">Commander</h5>
+			<p class="categoriesText">Nous offrons aussi un service de commande en ligne pour vous permettre de déguster sans avoir à vous déplacer</p>
+
+		</div>
+
+		<div class="elementCategories" id="">
+			<img src="assets/img/location.png" width="90px">
+			<h5 class="categoriesTitle">Louer un espace</h5>
+			<p class="categoriesText">Que vous soyez un chef professionnel ou un passionné de cuisine, nos espaces de cuisine à louer équipés de matériel de qualité sont la solution idéale pour réaliser vos projets</p>
+		</div>
+	</div>
+	<div class="containerSubscription">
+		<div class="elementSubscription" id="FreeSubscription">
+			<img class="imgSub" src="assets/img/free_sub.png" width="100px">
+			<h5 class="subscriptionTitle">Free</h5>
+			<ul>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;1 Leçons / jour</li>
+
+			</ul>  
+		</div>
+		<div class="elementSubscription" id="StarterSubscription">
+			<img class="imgSub" src="assets/img/starter_sub.png" width="100px">
+			<h5 class="subscriptionTitle">Starter</h5>
+			<ul>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;5 leçons par jour</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Aucun Pub</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Service de livraison offerte ! &nbsp;&nbsp;&nbsp;&nbsp;(uniquement en point relai)</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Invitation à des événements &nbsp;&nbsp;&nbsp;&nbsp;exclusifs (dégustations, rencontre &nbsp;&nbsp;&nbsp;&nbsp;avec des chefs, ventre privées...) </li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Chèque cadeau de 5€ tous les 3 
+				&nbsp;&nbsp;&nbsp;&nbsp;nouveaux inscrits (hors formule 
+				&nbsp;&nbsp;&nbsp;&nbsp;Free)</li>
+			</ul>  
+			
+		</div>
+		<div class="elementSubscription" id="MasterSubscription">
+			<img class="imgSub" src="assets/img/master_sub.png" width="100px">
+			<h5 class="subscriptionTitle">Master</h5>
+			<ul>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Leçons illimitées</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Réduction permanente de 5% sur 
+				&nbsp;&nbsp;&nbsp;&nbsp;la boutique</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Service de livraison offerte !</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Accès au service de location 
+				&nbsp;&nbsp;&nbsp;&nbsp;d’espace de cuisine</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Chèque cadeau de 5€ tous les 3 
+				&nbsp;&nbsp;&nbsp;&nbsp;nouveaux inscrits (hors formule 
+				&nbsp;&nbsp;&nbsp;&nbsp;Free + bonus de 3% du montant 
+				&nbsp;&nbsp;&nbsp;&nbsp;sur le total de la première 
+				&nbsp;&nbsp;&nbsp;&nbsp;commande du nouvel inscrit</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;Réduction de 10% du montant de 
+				&nbsp;&nbsp;&nbsp;&nbsp;l’abonnement en cas de 
+				&nbsp;&nbsp;&nbsp;&nbsp;renouvellement, valable uniquement 
+				&nbsp;&nbsp;&nbsp;&nbsp;sur le tarif annuel.</li>
+			</ul>
+			<br>
+			<p><strong>+ tous les avantages de l'abonnement Starter</strong></p>
+
+			
+		</div>
+	</div>
+
+	<div class="containerMobileApp">
+
+		<div class="mobileAppDownloadText">
+
+			<h5>Télécharger l'application</h5>
+			<h6>Pour accéder à vos cours</h6>
+		</div>
+
+		<div>
+			<img src="assets/img/location.png" width="200px">
+		</div>
+
+	</div>
+
+
+
+<!--FOOTER !-->
+
+</body>
+</html>
