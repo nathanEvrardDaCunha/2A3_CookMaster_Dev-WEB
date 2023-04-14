e34acf97e6e80ed8abd9b210376c4bb41ee42382
nathan <nathan.evrarddacunha@gmail.com>
2023-04-14 14:16:36 +0200

    [WIP] Création d'un Routeur p1

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,33 @@
 <?php
 
-require "../vendor/autoload.php";
+//use Router\Router;
 
-use Classes\Payment\Stripe;
+use Router\Router;
 
-$test = new Stripe();
+require __DIR__ . "/../vendor/autoload.php";
+//require_once __DIR__ . '/../Router/Router.php';
+
+//use Classes\Payment\Stripe;
+
+//$test = new Stripe();
+
+echo "0";
+
+
+$router = new Router();
+
+echo "1";
+
+
+/*$router->register('/', function(){
+    return 'HomePage';
+});*/
+
+/*$router->register('/contact', function(){
+    return 'ContactPage';
+});*/
+
+echo "2";
+echo "\n" . __DIR__ . "/../Router/Router.php";
+
+//echo $router->resolve($_SERVER['REQUEST_URI']);
