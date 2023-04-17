e63f269b641950372acc5121758b85658c3944dc
nathan <nathan.evrarddacunha@gmail.com>
2023-04-17 18:56:11 +0200

    [WIP] Amélioration du Routeur et ajout Contrôleur

--- Public/index.php
+++ Public/index.php
@@ -1,33 +1,24 @@
 <?php
 
-//use Router\Router;
-
 use Router\Router;
 
 require __DIR__ . "/../vendor/autoload.php";
-//require_once __DIR__ . '/../Router/Router.php';
-
-//use Classes\Payment\Stripe;
-
-//$test = new Stripe();
-
-echo "0";
-
 
 $router = new Router();
 
-echo "1";
 
+$router->register('/', ['Controllers\HomeController', 'index']);
 
-$router->register('/', function(){
-    return 'HomePage';
-});
-
-$router->register('/contact', function(){
+/*$router->register('/contact', function(){
     return 'ContactPage';
-});
+});*/
 
-echo "2";
-echo "\n" . __DIR__ . "/../Router/Router.php";
+//echo "\n" . __DIR__ . "/../Router/Router.php\n";
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+//var_dump($_SERVER['REQUEST_URI']);
+
+try{
+    echo $router->resolve($_SERVER['REQUEST_URI']);
+} catch (Exception $e){
+    echo $e->getMessage();
+}
